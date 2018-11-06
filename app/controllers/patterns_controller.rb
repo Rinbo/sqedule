daeead4ba8893db0448f110a0cd957e45ac94f54
rinbo <robin.b@outlook.com>
2018-11-06 15:45:04 +0100

    Add create action for patterns

--- app/controllers/patterns_controller.rb
+++ app/controllers/patterns_controller.rb
@@ -1,4 +1,20 @@
 class PatternsController < ApplicationController
   def create
+    @schedule = Schedule.find(params[:schedule_id])
+    @pattern = @schedule.patterns.create(pattern_params)
+    
+    if @pattern.persisted?
+      redirect_to schedule_path(@schedule)
+      flash[:notice] = "New pattern was successfully created"
+    else
+      render :new
+      flash[:notice] = "Something went wrong"
+    end
+  end
+
+  private
+
+  def pattern_params
+    params.require(:pattern).permit(:schedule_id, :shift_start, :shift_length, :break_length, :cleaning)
   end
 end
