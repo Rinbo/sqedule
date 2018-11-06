117f3fa7c0872988a5293a392c80bbf2f5bcf683
danmalmx <danmalmx@gmail.com>
2018-11-06 14:13:49 +0100

    getting modal to work locally after pulling down

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -44,9 +44,10 @@ let defaultConfig = require('tailwindcss/defaultConfig')()
 */
 
 let colors = {
+
   'smoke-darkest': 'rgba(0, 0, 0, 0.9)',
-  'smoke-darker': 'rgba(0, 0, 0, 0.75)',
-  'smoke-dark': 'rgba(0, 0, 0, 0.6)',
+  'smoke-darker': 'rgba(0, 0, 0, 0.5)',
+  'smoke-dark': 'rgba(0, 0, 0, 0.4)',
   'smoke': 'rgba(0, 0, 0, 0.5)',
   'smoke-light': 'rgba(0, 0, 0, 0.4)',
   'smoke-lighter': 'rgba(0, 0, 0, 0.25)',
