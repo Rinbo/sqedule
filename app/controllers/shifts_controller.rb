958ce87ecd985f4c5ddaff6f27a2b628d5a31546
rinbo <robin.b@outlook.com>
2018-11-08 14:40:33 +0100

    Refactor to create a new shift if non exists

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -2,4 +2,26 @@ class ShiftsController < ApplicationController
   def show
     @shift = Shift.find(params[:id])
   end
+
+  def new
+    @shift = Shift.new
+  end
+
+  def create
+    binding.pry
+    @shift = @schedule.pattern.shifts.create(shift_params)
+    if @shift.persisted?
+      redirect_to @schedule
+      flash[:notice] = "New schedule successfully created"
+    else
+      render :new
+      flash[:notice] = "Something went wrong"
+    end
+  end
+
+  private
+
+  def shift_params
+    params.require(:shift).permit(:date, :shift_start, :shift_length, :break_length, :cleaning)
+  end
 end
