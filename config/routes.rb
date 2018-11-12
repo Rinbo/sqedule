5e54a61c70c9294e33759ab0ee5f06d8f308c34d
rinbo <robin.b@outlook.com>
2018-11-12 11:45:28 +0100

    Modify routes with new action for creating staff

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :shifts, only: [:show, :new, :create, :edit, :update]
   end
 
-  resources :staffs, only: [:create] do
+  resources :staffs, only: [:create, :new] do
     resources :assignments, only: [:create, :new, :edit, :update]
   end
 
