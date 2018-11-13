7fb0be5b70ae53101d1f49e91e4c055cfccd3f3b
Albert Stjärne <albert.stjarne@gmail.com>
2018-11-13 15:29:17 +0100

    adds Delete staff link to staff show page, adds destroy method to staffs controller, adds destroy dependency to staff.rb, adds destroy to staffs in routes

--- app/controllers/staffs_controller.rb
+++ app/controllers/staffs_controller.rb
@@ -15,6 +15,17 @@ class StaffsController < ApplicationController
     end
   end
 
+  def destroy
+    @staff = Staff.find(params[:id])
+    if @staff.destroy
+      redirect_to root_path
+      flash[:notice] = "Staff was successfully deleted."
+    else
+      render 'show'
+      flash[:notice] = "Something went wrong. Staff was not deleted."
+    end
+  end
+
   private
 
   def staff_params
