1c3475e5bcaa7053e73ba4ad645d19ea1d67647a
rinbo <robin.b@outlook.com>
2018-11-16 09:12:56 +0100

    Clean up code

--- features/step_definitions/assertion_steps.rb
+++ features/step_definitions/assertion_steps.rb
@@ -16,8 +16,6 @@ Then("I am on assignment page") do
 end
 
 Then("I choose {string} on {string}") do |choice, dropdown|
-  #find(choice).find(dropdown).select_option
-  # find(choice, 'OFF').click dropdown
   select choice, from: dropdown
 end
 
